f4fa89588a17dd3827c6f0ea9c4cf82b732b572e
NuneSriDurga <163735964+NuneSriDurga@users.noreply.github.com>
2025-10-22 14:35:56 +0530

    Update CarManager.jsx

--- CAR-REACT/src/components/CarManager.jsx
+++ CAR-REACT/src/components/CarManager.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import config from './config.js';
+// import config from './config.js';
 import './style.css'; 
 
 const CarManager = () => {
